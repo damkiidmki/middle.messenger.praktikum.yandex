e6f26e2bdd5a93da472193a1e8037188b2f73776
123 <damkiidmki@yandex.ru>
2024-04-08 18:15:42 +0300

    Перезаписал всю сборку.

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path';
+import {defineConfig} from "vite";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                authorization: resolve(__dirname, 'src/pages/authorization/index.html'),
+                registration: resolve(__dirname, 'src/pages/registration/index.html'),
+                chat: resolve(__dirname, 'src/pages/chat/index.html'),
+                settings: resolve(__dirname, 'src/pages/settings/index.html'),
+                error_404: resolve(__dirname, 'src/pages/error/404/index.html'),
+                error_500: resolve(__dirname, 'src/pages/error/500/index.html')
+            }
+        }
+    },
+    plugins: [handlebars({
+        partialDirectory: resolve(__dirname, 'src/partials')
+    })]
+})
